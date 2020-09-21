8f8002c57168a9075a189f25c97801cea111ee97
Tallons <Tallons27@yahoo.com>
2020-09-21 00:12:09 -0600

    Bitwise & Input

--- src/Basics/Input.java
+++ src/Basics/Input.java
@@ -1,5 +1,16 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class Input {
 
+    public static void input() {
+        System.out.println("Enter your name");
+
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+
+        System.out.println("Nice to meet you " + name);
+        in.close();
+    }
 }
--- src/Basics/Operators/Bitwise.java
+++ src/Basics/Operators/Bitwise.java
@@ -8,5 +8,12 @@ public class Bitwise {
         System.out.println("XOR: " + (num1 ^ num2));
         System.out.println("NOT " + num1 + ": " + ~num1);
         System.out.println("NOT " + num2 + ": " + ~num2);
+        System.out.println("LEFT SHIFT " + num1 + ": " + (num1<<1));
+        // each left shift is multiplying number by 2
+        System.out.println("RIGHT SHIFT " + num1 + ": " + (num1>>1));
+        // each right shift is dividing number by 2 (rounds down)
+        System.out.println("LOGICAL SHIFT " + num1 + ": " + (num1>>>1));
+        // each logical shift is dividing number by 2 (rounds down)
+        // negatives are treated differently with logical shift
     }
 }
